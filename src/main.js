9be761ecce49f64b721c74e7c8b13e56ca922647
Enrico Bianchi <enrico.bianchi@gmail.com>
2019-10-07 00:28:48 +0200

    Added router library

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import router from './router';
+import VueRouter from "vue-router";
 
 Vue.config.productionTip = false;
+
 Vue.use(Vuetify);
+Vue.use((VueRouter));
+
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import VueRouter from 'vue-router';
+//import Home from "./views/Home";
+//import Hello from "./views/Hello";
+//import Albi from "./views/Albi";
+
+const routes = [
+    // {path: "/", component: Home, name: "FUMETTI_HOME"},
+    // {path: "/hello", component: Hello},
+    // {path: "/albi", component: Albi, name: "FUMETTI_ALBI"},
+];
+
+const router = new VueRouter({
+    routes
+});
+
+export default router;
