b52fea0d69e601b52903d59a41e64bbd10fb89e7
Enrico Bianchi <enrico.bianchi@gmail.com>
2019-10-07 00:57:49 +0200

    Added home routing page

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import VueRouter from 'vue-router';
-//import Home from "./views/Home";
+import Home from "./views/Home";
 //import Hello from "./views/Hello";
 //import Albi from "./views/Albi";
 
 const routes = [
-    // {path: "/", component: Home, name: "FUMETTI_HOME"},
-    // {path: "/hello", component: Hello},
+    {path: "/", component: Home, name: "FUMETTI_HOME"},
+    //{path: "/hello", component: Hello},
     // {path: "/albi", component: Albi, name: "FUMETTI_ALBI"},
 ];
 
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container fluid>
+        <v-layout row wrap>
+            <v-flex>
+                <v-card>
+                    <v-card-title><h1>Home Page!</h1></v-card-title>
+                    <v-card-text>Benvenuto!</v-card-text>
+                    <v-card-actions>
+                        <v-btn color="primary" @click="goToSearch">
+                            <v-icon>fas fa-search</v-icon>
+                            Ricerca
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        methods: {
+            goToSearch: function () {
+                this.$router.push({name: "FUMETTI_ALBI"})
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
