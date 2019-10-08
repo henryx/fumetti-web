2b7bf86837dbeeb6d8048a32408777d6b143c3ef
Enrico Bianchi <enrico.bianchi@antaresvision.com>
2019-10-08 10:58:18 +0200

    Fixed diaolog's layout

--- src/components/AddAlbiDialog.vue
+++ src/components/AddAlbiDialog.vue
@@ -1,54 +1,51 @@
 <template>
-    <v-row justify="center">
+    <v-layout row justify-center>
         <v-dialog v-model="dialog" persistent max-width="600px">
             <v-card>
                 <v-card-title>
                     <span class="headline">Aggiungi albo</span>
                 </v-card-title>
                 <v-card-text>
-                    <v-container>
-                        <v-row>
-                            <v-col cols="12" sm="6" md="4">
+                    <v-container grid-list-md>
+                        <v-layout wrap>
+                            <v-flex xs12 sm6 md4>
                                 <v-select label="Serie*" :items="serieItems" required></v-select>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
                                 <v-text-field label="Numero" type="number" @keypress="isNumber"></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field label="Data di pubblicazione*" required></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="8">
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
                                 <v-text-field label="Prezzo*" required type="number" @keypress="isNumber"></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="4">
-                                <v-text-field label="Valuta*"></v-text-field>
-                            </v-col>
-                            <v-col cols="12" sm="6" md="6">
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
+                                <v-text-field label="Valuta*" required></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6>
                                 <v-select
-                                        :items="items"
+                                        :items="['0-17', '18-29', '30-54', '54+']"
                                         label="Age*"
                                         required
                                 ></v-select>
-                            </v-col>
-                            <v-col cols="12" sm="6">
+                            </v-flex>
+                            <v-flex xs12 sm6>
                                 <v-autocomplete
                                         :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
                                         label="Interests"
                                         multiple
                                 ></v-autocomplete>
-                            </v-col>
-                        </v-row>
+                            </v-flex>
+                        </v-layout>
                     </v-container>
                     <small>*indicates required field</small>
                 </v-card-text>
                 <v-card-actions>
-                    <div class="flex-grow-1"></div>
-                    <v-btn color="blue" text @click="dialog = false">Close</v-btn>
-                    <v-btn color="blue" text @click="saveAlbo">Save</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
+                    <v-btn color="blue darken-1" flat @click="dialog = false">Save</v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
-    </v-row>
+    </v-layout>
 </template>
 
 <script>
