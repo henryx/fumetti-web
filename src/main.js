055cd8cdb85c5db6e23e3202fa7ffbd6c3321e27
Enrico Bianchi <enrico.bianchi@gmail.com>
2019-10-23 00:24:17 +0200

    Fixed import orders

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
 import router from './router';
 import VueRouter from "vue-router";
 
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'material-design-icons/iconfont/material-icons.css'
+import 'vuetify/dist/vuetify.min.css';
+
 import 'typeface-roboto/index.css'
 
 Vue.config.productionTip = false;
