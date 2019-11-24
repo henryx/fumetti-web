fca3666ecd141f6c63a45db3a075059b7af009e9
Enrico Bianchi <enrico.bianchi@gmail.com>
2019-11-24 22:50:52 +0100

    Added api to execute POST for serie

--- src/api/serie.js
+++ src/api/serie.js
@@ -3,5 +3,9 @@ import Api from "./api";
 export default {
     async getSerie() {
         return Api().get("/serie");
+    },
+
+    async postSerie(data) {
+        return Api().post("/serie", data)
     }
 }
